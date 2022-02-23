034e6f01988d99984862b0f04e74207fa6babac3
Aleen <aleen42@vip.qq.com>
2022-02-23 18:41:27 +0800

    for test

--- index.js
+++ index.js
@@ -1,110 +1,62 @@
-var q = require('q');
-//var webdriver = require('selenium-webdriver');
+function SeleniumWebdriverBrowser(baseBrowserDecorator, args, logger) {
+    const self = this, log = logger.create('launcher.selenium-webdriver');
+    baseBrowserDecorator(self);
 
-function SeleniumWebdriverBrowser(id, baseBrowserDecorator, args, logger) {
-  baseBrowserDecorator(this);
+    let killingPromise;
 
-  var self = this;
-  var captured = false;
-  var log = logger.create('launcher.selenium-webdriver');
-  var browserName = args.browserName;
-  var killingPromise;
+    self._start = url => {
+        args.getDriver().then(function (driver) {
+            self.driver_ = driver;
+            driver.get(url);
+        });
+    };
 
-  self.id = id;
-  self.setName(browserName);
+    self.kill = () => {
+        // Already killed, or being killed.
+        return (killingPromise = (killingPromise || Promise.resolve()).then(() => new Promise(resolve => {
+            self.getSession_(session => {
+                if (session) {
+                    log.info('requested to kill, session id is ' + (session.id_));
 
-  log.info('SeleniumWebdriverBrowser (kid:'+id+') created');
+                    if (session && session.id_) {
+                        self.driver_ && self.driver_.quit();
+                        resolve();
+                    }
+                }
+            });
+        })));
+    };
 
-  self._start = function(url){
-    log.info('starting '+self.name);
-    var driver = args.getDriver();
-    self.driver_ = driver;
-
-    self.getSession_(function(session){
-      // TODO: caps_ might be a defer as well
-      self.setName(session.caps_.caps_);
-    });
-
-    log.info('sending driver to url '+url);
-    driver.get(url);
-  };
-
-  self.kill = function(){
-
-    // Already killed, or being killed.
-    if (killingPromise) {
-      return killingPromise;
-    }
-
-    var deferred = q.defer();
-
-    killingPromise = deferred.promise;
-
-    self.getSession_(function(session){
-      log.info('requested to kill, session id is '+(session && session.id_));
-
-      if (!session) {
-        return deferred.reject();
-      }
-
-      if(session.id_){
-        self.driver_ && self.driver_.quit();
-        deferred.resolve();
-      }
-    });
-
-    return killingPromise;
-
-  };
-
-  self.forceKill = function() {
-    self.kill();
-    return killingPromise;
-  };
+    self.forceKill = () => {
+        self.kill();
+        return killingPromise;
+    };
 
 }
 
-SeleniumWebdriverBrowser.$inject = [ 'id', 'baseBrowserDecorator', 'args', 'logger' ];
+SeleniumWebdriverBrowser.$inject = ['baseBrowserDecorator', 'args', 'logger'];
 
-SeleniumWebdriverBrowser.prototype.setName = function(arg) {
-  // arg is either string and assumed to be `browserName` parameter
-  // or is object, being the session capabilities
-  var browserName;
-  if('string' === typeof arg){
-    browserName = arg;
-  }else if(arg && arg.version){
-    browserName = (arg.browserName || arg.device) + (arg.version ? ' ' + arg.version : '') + ' (' + arg.platform +  ')';
-  }
+Object.assign(SeleniumWebdriverBrowser.prototype, {
+    getSession_(cb) {
+        const driver = this.driver_;
 
-  if(browserName){
-    this.name = browserName + ' via Selenium Webdriver';
-    console.log('changed name to '+this.name);
-  }
-};
+        if (!driver) {
+            return cb(null);
+        }
 
-SeleniumWebdriverBrowser.prototype.getSession_ = function(cb) {
-  var driver = this.driver_;
+        if (driver.session_ && driver.session_.then) {
+            driver.session_.then(cb);
+        } else {
+            cb(driver.session_);
+        }
+    },
 
-  if(!driver){
-    return cb(null);
-  }
-
-  if(driver.session_ && driver.session_.then){
-    driver.session_.then(cb);
-  }else{
-    cb(driver.session_);
-  }
-};
-
-SeleniumWebdriverBrowser.prototype.isCaptured = function(){
-  return !!this.driver_;
-};
-
-SeleniumWebdriverBrowser.prototype.toString = function() {
-  return this.name || 'Unnamed SeleniumWebdriverBrowser';
-};
+    isCaptured() {
+        return !!this.driver_;
+    },
+});
 
 // PUBLISH DI MODULE
 module.exports = {
-  'launcher:SeleniumWebdriver': ['type', SeleniumWebdriverBrowser]
+    'launcher:selenium-webdriver' : ['type', SeleniumWebdriverBrowser],
 };
